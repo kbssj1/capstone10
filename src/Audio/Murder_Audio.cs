37a1685c5e4f0d0c15eef6e96b206157279060ad
kbssj1 <kbssj1@naver.com>
2017-10-11 10:29:39 +0900

    변수명 변경

--- src/Audio/Murder_Audio.cs
+++ src/Audio/Murder_Audio.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Murder_Audio : AudioController
 {
-    public AudioClip Audio_Run;
-    public AudioClip Audio_Walk;
-    public AudioClip Audio_Attack_cry;
-    public AudioClip Audio_Death;
+    public AudioClip AUDIO_RUN;
+    public AudioClip AUDIO_WALK;
+    public AudioClip AUDIO_ATTACK_CRY;
+    public AudioClip AUDIO_DEATH;
     private float playTime = 1.0f;
-    public bool RepeatCheck_Death = true;
+    public bool checkMurderDeath = true;
 
     void Start()
     {
@@ -18,55 +18,53 @@ public class Murder_Audio : AudioController
 
     public void PlayAudio(AudioType audiotype, bool isOneSound = false)
     {
-        if (audioSource != null)
+        if (isOneSound == false)
         {
-            if (isOneSound == false)
+            switch (audiotype)
             {
-                switch (audiotype)
-                {
-                    case AudioType.MURDER_RUN:
-                        audioSource.clip = Audio_Run;
-                        break;
-                    case AudioType.MURDER_WALK:
-                        audioSource.clip = Audio_Walk;
-                        break;
-                    case AudioType.NOT:
-                        audioSource.clip = null;
-                        audioSource.Stop();
-                        break;
-                    default:
-                        Debug.LogError("잘못된 오디오 명을 입력하셨습니다.(Murder)");
-                        break;
-                }
-                PlayCurrentAudio();
+                case AudioType.MURDER_RUN:
+                    audioSource.clip = AUDIO_RUN;
+                    break;
+                case AudioType.MURDER_WALK:
+                    audioSource.clip = AUDIO_WALK;
+                    break;
+                case AudioType.NOT:
+                    audioSource.clip = null;
+                    audioSource.Stop();
+                    break;
+                default:
+                    Debug.LogError("잘못된 오디오 명을 입력하셨습니다.(Murder)");
+                    break;
             }
-            else
+            PlayCurrentAudio();
+        }
+        else
+        {
+            switch (audiotype)
             {
-                switch (audiotype)
-                {
-                    case AudioType.MURDER_ATTACK:
-                        if (GetCheck())
-                        { 
-                            audioSource.clip = Audio_Attack_cry;
-                            playTime = 1.2f;
-                        }
-                        break;
-                    case AudioType.MURDER_DEATH:
-                        if (RepeatCheck_Death == true)
-                        {
-                            audioSource.clip = Audio_Death;
-                            RepeatCheck_Death = false;
-                            playTime = audioSource.clip.length;
-                        }
-                        break;
-                    default:
-                        Debug.LogError("잘못된 오디오 명을 입력하셨습니다.(Survivor)");
-                        break;
-                }
-                // PlayCurrentAudioRightly();             
-                if (audioSource.clip != null)
-                    StartCoroutine(TestAudio(playTime));
+                case AudioType.MURDER_ATTACK:
+                    if (isAudioPlay())
+                    { 
+                        audioSource.clip = AUDIO_ATTACK_CRY;
+                        playTime = 1.2f;
+                    }
+                    break;
+                case AudioType.MURDER_DEATH:
+                    if (checkMurderDeath == true)
+                    {
+                        audioSource.clip = AUDIO_DEATH;
+                        checkMurderDeath = false;
+                        playTime = audioSource.clip.length;
+                    }
+                    break;
+                default:
+                    Debug.LogError("잘못된 오디오 명을 입력하셨습니다.(Survivor)");
+                    break;
             }
+            // PlayCurrentAudioRightly();             
+            if (audioSource.clip != null)
+                StartCoroutine(TestAudio(playTime));
         }
     }
+    
 }
